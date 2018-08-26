074ef8a14cb1085afc089a0b44fad00b6ef8c408
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2018-08-26 17:47:23 +0200

    start creating React generator

---
+++ scripts/reactify.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+
+reactify();
+
+function reactify() {
+    const cssCollection = {};
+
+    grabCssDetails();
+
+    Object.keys(cssCollection).forEach(collectionToReact);
+
+    function grabCssDetails() {
+        let cssContent = fs.readFileSync('./alegrify-ui.css').toString();
+        const regex = /alegrify-([a-z0-9-]+)(?:__)?([a-z0-9-]+)?(?:\:)?([a-z0-9-]+)?(?:\[)?([a-z0-9-]+)?(?:\])?/;
+    
+        while (cssContent.match(regex)) {
+            const match = cssContent.match(regex);
+            const nameAndVariant = match[1];
+            const [name, variant] = nameAndVariant.split('--');
+            cssCollection[name] = cssCollection[name] || {
+                variants: [],
+                states: [],
+                children: [],
+                pseudo: []
+            };
+    
+            if (variant) {
+                if (cssCollection[name].variants.indexOf(variant) === -1) {
+                    cssCollection[name].variants.push(variant);
+                }
+            }
+    
+            const map = [null, null, 'children', 'pseudo', 'states'];
+            for (let i = 2; i < 5; i++) {
+                if (match[i] && cssCollection[name][map[i]].indexOf(match[i]) === -1) {
+                    cssCollection[name][map[i]].push(match[i]);
+                }
+            }
+    
+    
+            cssContent = cssContent.replace(regex, '');
+        }
+    }
+
+
+    function collectionToReact(component) {
+        const { variants, states, children, pseudo } = cssCollection[component];
+
+        
+
+        const template = `
+/**
+ * 
+ */ 
+function ${camelizeString(component)} () {
+
+}        
+        `;
+
+        console.log(template);
+    }
+
+    function camelizeString(string) {
+        return string
+            .split('-')
+            .map(word => word.length ? word.replace(word[0], word[0].toUpperCase()) : '')
+            .join('');
+    }
+
+}
