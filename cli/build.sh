c878fd3fa87963bf222187b2208d422e951350e9
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-02 21:40:51 +0200

    themes: article and dark

--- cli/build.sh
+++ cli/build.sh
@@ -1,7 +1,7 @@
 lessc index.less alegrify-ui.css --auto-rtl --autoprefix --global-var="theme=default"
 lessc alegrify-ui.css alegrify-ui.min.css --clean-css
 
-for THEME in "article"
+for THEME in "article" "dark"
 do
   lessc index.less alegrify-ui--$THEME.css --auto-rtl --autoprefix --global-var="theme=$THEME"
   lessc alegrify-ui.css alegrify-ui--$THEME.min.css --clean-css
