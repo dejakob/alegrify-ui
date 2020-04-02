8ed9d856450b836320d866215e5e5b5058a3c126
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-02 20:22:39 +0200

    Start adding themes

---
+++ cli/build.sh
@@ -0,0 +1,8 @@
+lessc index.less alegrify-ui.css --auto-rtl --autoprefix --global-var="theme=default"
+lessc alegrify-ui.css alegrify-ui.min.css --clean-css
+
+for THEME in "article"
+do
+  lessc index.less alegrify-ui--$THEME.css --auto-rtl --autoprefix --global-var="theme=$THEME"
+  lessc alegrify-ui.css alegrify-ui--$THEME.min.css --clean-css
+done
