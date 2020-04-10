b0c242778d43317208b7fe0d52dca757c8688552
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-10 16:04:24 +0200

    minify theme files

--- cli/build.sh
+++ cli/build.sh
@@ -4,5 +4,5 @@ lessc alegrify-ui.css alegrify-ui.min.css --clean-css
 for THEME in "article" "dark"
 do
   lessc index.less alegrify-ui--$THEME.css --auto-rtl --autoprefix --global-var="theme=$THEME"
-  lessc alegrify-ui.css alegrify-ui--$THEME.min.css --clean-css
+  lessc alegrify-ui--$THEME.css alegrify-ui--$THEME.min.css --clean-css
 done
